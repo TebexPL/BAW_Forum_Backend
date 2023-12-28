586a9d30c35423829efa8319eed6cb9444b81394
TebexPL <tomebe1212@gmail.com>
2023-12-28 13:40:23 +0100

    WIP

--- index.js
+++ index.js
@@ -1,14 +1,44 @@
 import 'dotenv/config';
 import pg from 'pg';
 import express from 'express';
-import cookieSession from 'cookie-session';
+import cookieParser from 'cookie-parser';
 import expressSession from 'express-session';
 
 const client = new pg.Client();
+await client.connect();
 
 const app = express();
 
+const cookieManagement = (req, res, next) => {
+  if(!req.cookies['session'])
+      res.cookie("session", 'token', { expires: new Date(Date.now() + 900000), httpOnly: true });
+  else{
+    const id = req.cookies['session'];
+    const userSession = client.query("SELECT * FROM sessions WHERE session = $1", [id]);
+
+
+
+  }
+  next();
+}
+
+
+
+
+
+
+
+
+
+
+
+app.use(cookieParser());
+
+app.use(cookieManagement);
+
 app.use(express.static('./public'));
+
+
 
 
 app.listen(process.env.PORT);
